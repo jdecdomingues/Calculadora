1fdd3d93b8bfe024f4f3bf4bb28daa9dd9c36020
jdecdomingues <jdecdomingues@users.noreply.github.com>
2025-09-17 00:09:07 -0300

    Calculadora C#

---
+++ Models/Calculadora.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Calculadora;
+
+public class Calculadora
+ {
+ 
+ public void Somar(double valor1, double valor2)
+ {
+  Console.WriteLine($"{valor1} + {valor2} =  {valor1 + valor2}");
+ }   
+ 
+ public void Subtrair(double valor1, double valor2)
+ {
+  Console.WriteLine($"{valor1} - {valor2} =  {valor1 - valor2}");
+ }   
+ 
+ public void Mutiplicar(double valor1, double valor2)
+ {
+  Console.WriteLine($"{valor1} * {valor2} =  {valor1 * valor2}");
+ }   
+ 
+ public void Dividir(double valor1, double valor2)
+ {
+  Console.WriteLine($"{valor1} / {valor2} =  {valor1 / valor2}");
+ }  
+ 
+ public void Potencia(double valor1, double valor2)
+ {
+  double potencia = Math.Pow(valor1, valor2);
+  Console.WriteLine($"{valor1} ^ {valor2} = {potencia}");
+ }  
+ 
+ public void RaizQuadrada(double valor1)
+ {
+  double raizQuadrada = Math.Sqrt(valor1);
+  Console.WriteLine($"Raiz quadrada de {valor1}  = {raizQuadrada}");
+ }  
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Xml.Serialization;
+
+using Calculadora;
+
+Calculadora.Calculadora calculadora = new Calculadora.Calculadora();
+
+double valor1 = 0;
+double valor2 = 0;
+
+Console.WriteLine("Digite a operação que deseja realizar de acordo com as opções abaixo:");
+
+ArrayList operacoes = new ArrayList();
+operacoes.Add("Somar");
+operacoes.Add("Subtrair");
+operacoes.Add("Multiplicar");
+operacoes.Add("Dividir");
+operacoes.Add("Potencia");
+operacoes.Add("Raiz");
+
+for (int i = 0; i < operacoes.Count; i++)
+{
+    Console.WriteLine(operacoes[i]);
+   
+}
+Console.WriteLine("\n");
+
+string operacaoEscolhida = Console.ReadLine();
+
+if (operacaoEscolhida.Equals("Raiz")) {
+    
+    Console.WriteLine("Agora digite o primeiro valor da operação");
+    valor1 = Convert.ToDouble(Console.ReadLine());
+    
+} else {
+    
+    Console.WriteLine("Agora digite o primeiro valor da operação");
+    valor1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Agora digite o segundo valor da operação");
+    valor2 = Convert.ToDouble(Console.ReadLine());
+        
+}
+
+switch (operacaoEscolhida)
+{
+    case "Somar":
+        calculadora.Somar(valor1,valor2);
+        break;
+    case "Multiplicar":
+        calculadora.Mutiplicar(valor1,valor2);
+        break;
+    case "Dividir":
+        calculadora.Dividir(valor1,valor2);
+        break;
+    case "Subtrair":
+        calculadora.Subtrair(valor1,valor2);
+        break;
+    case "Potencia":
+        calculadora.Potencia(valor1,valor2);
+        break;
+    case "Raiz":
+        calculadora.RaizQuadrada(valor1);
+        break;
+    
+    default:
+        Console.WriteLine("Operação não encontrada");
+        break;
+}
+
+
+ 
+
+
+
